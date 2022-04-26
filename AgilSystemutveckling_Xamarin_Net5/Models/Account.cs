83baf38b1abad3c66073e809225311688c9be354
Hanna Tekie <hantek90@gmail.com>
2022-04-26 13:51:29 +0200

    created Account class

---
+++ AgilSystemutveckling_Xamarin_Net5/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿namespace AgilSystemutveckling_Xamarin_Net5.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string userName { get; set; }
+        public string Password { get; set; }
+        public int Access { get; set; }
+        public string Address { get; set; }
+        public bool Blocked { get; set; }
+
+    }
+}
