3e45cb67c193d8dd9f1fba8dd1b24afb2be9334b
Mohamed EL Yahiaoui <Mohamed.el.yahiaoui@outlook.com>
2022-04-27 15:25:40 +0200

    Creation of all Classes in Models folder

---
+++ AgilSystemutveckling_Xamarin_Net5/Models/Products.cs
@@ -0,0 +1,12 @@
+﻿namespace AgilSystemutveckling_Xamarin_Net5.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Authors Author { get; set; }
+        public Categorys Category { get; set; }
+        public SubCategorys SubCategory { get; set; }
+    }
+}
