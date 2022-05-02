75cd6988278b52abd788d80709477e7ab69c3b49
Frans Nilsson Lidström <fransnilssonlidstrom@gmail.com>
2022-05-02 13:08:29 +0200

    SearchPage added searchbar and "list view"

---
+++ AgilSystemutveckling_Xamarin_Net5/Models/Books.cs
@@ -0,0 +1,7 @@
+﻿namespace AgilSystemutveckling_Xamarin_Net5.Models
+{
+    public class Books
+    {
+        public string Title { get; set; }
+    }
+}
--- AgilSystemutveckling_Xamarin_Net5/Pages/SearchPage.cshtml.cs
+++ AgilSystemutveckling_Xamarin_Net5/Pages/SearchPage.cshtml.cs
@@ -5,8 +5,15 @@ namespace AgilSystemutveckling_Xamarin_Net5.Pages
 {
     public class SearchPageModel : PageModel
     {
+        public List<Models.Books> BookName;
+
         public void OnGet()
         {
+            BookName = new List<Models.Books>() 
+            {
+                new Models.Books() { Title = "Harry Potter"}
+            };
+
         }
     }
 }
