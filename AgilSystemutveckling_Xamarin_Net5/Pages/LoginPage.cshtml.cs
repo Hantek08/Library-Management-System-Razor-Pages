4c756f5c09e810b4190015b45f75b68bb1bfc2ee
Vidar Fröberg Tinell <vidar.tinell@gmail.com>
2022-05-03 11:33:25 +0200

    Login page: can login

--- AgilSystemutveckling_Xamarin_Net5/Pages/LoginPage.cshtml.cs
+++ AgilSystemutveckling_Xamarin_Net5/Pages/LoginPage.cshtml.cs
@@ -5,8 +5,39 @@ namespace AgilSystemutveckling_Xamarin_Net5.Pages
 {
     public class LoginPageModel : PageModel
     {
+        [BindProperty]
+        public string Username { get; set; }
+
+        [BindProperty]
+        public string Password { get; set; }
+
+        public static string username;
+        public static string password;
+
         public void OnGet()
         {
+
+        }
+
+
+        public IActionResult OnPost()
+        {
+            Username = Request.Form["username"];
+            Password = Request.Form["password"];
+            username = Username;
+            password = Password;
+
+            if (Username.Equals("abc") && Password.Equals("123"))
+            {
+                /*HttpContext.Session.SetString("username", Username);
+                HttpContext.Session.SetString(Username, username);*/
+                return RedirectToPage("/UserPage");
+            }
+            else
+            {
+ 
+                return Page();
+            }
         }
     }
 }
