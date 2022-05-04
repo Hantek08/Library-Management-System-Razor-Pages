33a5dcd1ea23ca24ad7ba4e3a819e9e96fdd7a16
Mohamed EL Yahiaoui <Mohamed.el.yahiaoui@outlook.com>
2022-05-04 13:31:36 +0200

    Created AdminPage

---
+++ AgilSystemutveckling_Xamarin_Net5/Pages/AdminPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgilSystemutveckling_Xamarin_Net5.Pages
+{
+    public class AdminPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
