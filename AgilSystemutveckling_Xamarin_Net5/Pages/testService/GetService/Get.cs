797da059a71a10c653ac9b545bed40f18156c91b
Hector Wiklund <heckee@gmail.com>
2022-05-04 21:27:27 +0200

    Revert "Added sort methods."
    This reverts commit 2fa79a8d01fd5759bbd20bbc28317a240c24fe2e.

--- AgilSystemutveckling_Xamarin_Net5/Pages/testService/GetService/Get.cs
+++ AgilSystemutveckling_Xamarin_Net5/Pages/testService/GetService/Get.cs
@@ -15,13 +15,12 @@ namespace AgilSystemutveckling_Xamarin_Net5.Pages.TestService.GetService
 
         public static List<TestModels.Product> GetAllProducts()
         {
-            var sql = @$"SELECT Title, Author.AuthorName,
-                            Category.CategoryName, SubCategory.SubCategoryName
-                            from Product
-                            INNER JOIN Author ON Product.AuthorId = Author.Id
-                            INNER JOIN Category ON Product.CategoryId = Category.Id
-                            INNER JOIN SubCategory ON Product.SubCategoryId = SubCategory.Id";
-
+            var sql = @$"SELECT Title, Author.AuthorName, 
+                        Category.CategoryName, SubCategory.SubCategoryName
+                        from Product
+                        INNER JOIN Author ON Product.AuthorId = Author.Id
+                        INNER JOIN Category ON Product.CategoryId = Category.Id
+                        INNER JOIN SubCategory ON Product.SubCategoryId = SubCategory.Id";
             var name = new List<TestModels.Product>();
             using (var connection = new MySqlConnection(connString))
             {
@@ -32,11 +31,6 @@ namespace AgilSystemutveckling_Xamarin_Net5.Pages.TestService.GetService
             return name;
         }
 
-        #region Get methods
-        /// <summary>
-        /// Gets all users from MySql database, ordered by Id.
-        /// </summary>
-        /// <returns></returns>
         public static List<TestModels.User> GetAllUsers()
         {
             var sql = @$"Select Username, Password from User";
@@ -49,43 +43,6 @@ namespace AgilSystemutveckling_Xamarin_Net5.Pages.TestService.GetService
 
             return user;
         }
-        #endregion
-
-        #region Sort methods
-        /// <summary>
-        /// Get all users in database sorted in alphabetical order.
-        /// </summary>
-        /// <returns></returns>
-        public static List<TestModels.User> GetAllUsersInAlphabeticalOrder()
-        {
-            var sql = @$"select Username from User ORDER BY Username;";
-            var user = new List<TestModels.User>();
-            using (var connection = new MySqlConnection(connString))
-            {
-                connection.Open();
-                user = connection.Query<TestModels.User>(sql).ToList();
-            }
-
-            return user;
-        }
-
-        /// <summary>
-        /// Gets all users in reversed alphabetical order.
-        /// </summary>
-        /// <returns></returns>
-        public static List<TestModels.User> GetAllUsersInReversedOrder()
-        {
-            var sql = @$"Select Username from User ORDER BY Username desc;";
-            var user = new List<TestModels.User>();
-            using (var connection = new MySqlConnection(connString))
-            {
-                connection.Open();
-                user = connection.Query<TestModels.User>(sql).ToList();
-            }
-
-            return user;
-        }
-        #endregion
 
 
 
