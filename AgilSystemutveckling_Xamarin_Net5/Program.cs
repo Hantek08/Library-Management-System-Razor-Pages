9dc50d7c871024008e6e0f0d6b9af43f558665fc
Hanna Tekie <hantek90@gmail.com>
2022-05-04 15:14:37 +0200

    ..

--- AgilSystemutveckling_Xamarin_Net5/Program.cs
+++ AgilSystemutveckling_Xamarin_Net5/Program.cs
@@ -1,46 +1,10 @@
-using AgilSystemutveckling_Xamarin_Net5.Data;
-using Microsoft.EntityFrameworkCore;
-using MySqlConnector;
-using AgilSystemutveckling_Xamarin_Net5.TestModels;
-
-using AgilSystemutveckling_Xamarin_Net5;
-
-
-#region Test Data Shown in Console
-MySqlConnection connection = new MySqlConnection(
-    "Server=xamarindb.c6pefsvvniwb.eu-north-1.rds.amazonaws.com; Database=sys; UID=admin; Password=Xamarin321;"
-    );
-
-connection.Open();
-
-string query = "Select * from Item";
-
-
-//var query = AddProduct();
-MySqlCommand command = new MySqlCommand(query, connection);
-MySqlDataReader reader = command.ExecuteReader();
-
-while (reader.Read())
-{
-    Console.WriteLine(reader["Id"]);
-    Console.WriteLine(reader["Name"]);
-    
-}
-#endregion
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-// 
-builder.Services.AddDbContext<LibraryDbContext>(
-    options => options.UseMySql(
-        ServerVersion.AutoDetect("DefaultConnection")
-        )
-    );
 var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
