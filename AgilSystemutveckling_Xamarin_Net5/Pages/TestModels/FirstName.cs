88c5640b65f43c607e898679fab5218aa8ec7af6
Hector Wiklund <heckee@gmail.com>
2022-05-05 14:19:33 +0200

    Removed classes for names.

--- AgilSystemutveckling_Xamarin_Net5/Pages/TestModels/FirstName.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AgilSystemutveckling_Xamarin_Net5.TestModels
-{
-    public class FirstName
-    {
-        public int Id { get; set; }
-        public string? _FirstName { get; set; }
-    }
-}
--- AgilSystemutveckling_Xamarin_Net5/Pages/TestModels/FullName.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AgilSystemutveckling_Xamarin_Net5.TestModels
-{
-    public class FullName
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Surname { get; set; }
-    }
-}
