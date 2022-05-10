07762a2dbe9850fb84742ec10dcd4191912fea4c
Vidar Fröberg Tinell <vidar.tinell@gmail.com>
2022-05-10 16:13:35 +0200

    Description page almost done. Need to fix author

--- AgilSystemutveckling_Xamarin_Net5/Pages/DescriptionPage.cshtml.cs
+++ AgilSystemutveckling_Xamarin_Net5/Pages/DescriptionPage.cshtml.cs
@@ -5,8 +5,24 @@ namespace AgilSystemutveckling_Xamarin_Net5.Pages
 {
     public class DescriptionPageModel : PageModel
     {
+        public static TestModels.Product products;
+        public static string description;
+        public static string title;
+        public static string author;
+
         public void OnGet()
         {
+            products = TestService.GetService.Get.GetProductById(2);
+            description = products.Description;
+            title = products.Title;
+            author = products.AuthorName;
         }
+
+        public void OnPost()
+        {
+            ViewData["Books"] = description;
+            // git test
+        }
+
     }
 }
