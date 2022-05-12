0663cfc3da6ce108c6a1fe7d42b35f48bbd26cb6
Mohamed EL Yahiaoui <Mohamed.el.yahiaoui@outlook.com>
2022-05-13 00:16:30 +0200

    Fixed merge problems and made info button visble

--- AgilSystemutveckling_Xamarin_Net5/Program.cs
+++ AgilSystemutveckling_Xamarin_Net5/Program.cs
@@ -3,7 +3,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
