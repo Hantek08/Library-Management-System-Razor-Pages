28004eadfbcf08948f4750e0a8482a545cb1643f
Mohamed EL Yahiaoui <Mohamed.el.yahiaoui@outlook.com>
2022-05-18 16:02:14 +0200

    Fixat delite i Cart, fixat cart layout(Fattas check out) fixat lend book i searchpage, fixat bugg i logg out knappen, fixade lite i discription

--- AgilSystemutveckling_Xamarin_Net5/Globals.cs
+++ AgilSystemutveckling_Xamarin_Net5/Globals.cs
@@ -1,18 +1,18 @@
-﻿using AgilSystemutveckling_Xamarin_Net5.TestModels;
+﻿using AgilSystemutveckling_Xamarin_Net5.Models;
 
 namespace AgilSystemutveckling_Xamarin_Net5
 {
     public static class Globals
     {
         //Saves the information of the logged in User
-        static public User LoggedInUser = new User();
+        static public Users LoggedInUser = new Users();
 
         
         //static public Product ChosenProduct = new Product();
         
 
         //The users checkout list
-        static public List<Product> ChosenProduct = new List<Product>();
+        static public List<Products> CartList = new List<Products>();
 
         static public string Gstring = "";
     }
