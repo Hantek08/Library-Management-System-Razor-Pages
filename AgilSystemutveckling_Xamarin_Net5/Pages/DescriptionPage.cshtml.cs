93e2a1739ae99ae6c15514fc57535aaa55512644
Mohamed EL Yahiaoui <Mohamed.el.yahiaoui@outlook.com>
2022-05-18 21:50:42 +0200

    Changed classes from testmodels to models

--- AgilSystemutveckling_Xamarin_Net5/Pages/DescriptionPage.cshtml.cs
+++ AgilSystemutveckling_Xamarin_Net5/Pages/DescriptionPage.cshtml.cs
@@ -5,7 +5,7 @@ namespace AgilSystemutveckling_Xamarin_Net5.Pages
 {
     public class DescriptionPageModel : PageModel
     {
-        public static TestModels.Product product = new TestModels.Product();
+        public static Models.Products product = new Models.Products();
         public static string description;
         public static string title;
         public static string author;
@@ -13,7 +13,7 @@ namespace AgilSystemutveckling_Xamarin_Net5.Pages
 
         public void OnGet(int id)
         {
-            product = TestService.GetService.Get.GetProductById(id);
+            product = Service.GetService.Get.GetProductById(id);
             description = product.Description;
             title = product.Title;
             author = product.AuthorName;
@@ -28,7 +28,7 @@ namespace AgilSystemutveckling_Xamarin_Net5.Pages
         public void OnPostAddToCart()
         {
 
-            Globals.ChosenProduct.Add(product);
+            Globals.CartList.Add(product);
             Thread.Sleep(3000);
             //return RedirectToPage("/DescriptionPage", new { id = product.Id });
         }
