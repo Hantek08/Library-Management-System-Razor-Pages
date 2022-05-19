2c87a3ef2b5bd2661f85bfff479c0e854d0b7d1e
Sahar Ali <sahar.aliabdou@gmail.com>
2022-05-19 13:40:41 +0200

    added folder reports

--- AgilSystemutveckling_Xamarin_Net5/Pages/SaharsTest.cshtml.cs
+++ AgilSystemutveckling_Xamarin_Net5/Pages/SaharsTest.cshtml.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using AgilSystemutveckling_Xamarin_Net5.Service.ReportsService;
 namespace AgilSystemutveckling_Xamarin_Net5.Pages
 {
     public class SaharsTestModel : PageModel
@@ -12,7 +12,8 @@ namespace AgilSystemutveckling_Xamarin_Net5.Pages
         public void OnGet()
         {
 
-            Update.UpdateUnitsInStock(1, 25);
+            //Update.UpdateUnitsInStock(1, 25);
+            Reports.MostPopularBook();
         }
 
     }
---
+++ AgilSystemutveckling_Xamarin_Net5/Service/ReportsService/Reports.cs
@@ -0,0 +1,47 @@
+﻿using AgilSystemutveckling_Xamarin_Net5.Models;
+using Dapper;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using static AgilSystemutveckling_Xamarin_Net5.Constants.Constant;
+using AgilSystemutveckling_Xamarin_Net5.Service.GetService;
+
+
+
+namespace AgilSystemutveckling_Xamarin_Net5.Service.ReportsService
+{
+    public class Reports
+    {
+
+        public static List<History?> MostPopularBook()
+        {
+            string? sql = @$"SELECT Title,Count(Products.Id) as '# lent'
+			FROM History
+			INNER JOIN Actions on ActionId = Actions.Id
+			INNER JOIN Products on ProductId =  Products.Id
+			group by Title 
+            order by Count(Products.Id) desc limit 3
+            ";
+            using (var connection = new MySqlConnection(ConnectionString))
+            {
+                connection.Open();
+                if (connection.State == ConnectionState.Open)
+                {
+                    var history = connection.Query<History?>(sql).ToList();
+
+                    connection.Close();
+
+                    return history;
+                }
+            }
+
+            return null;
+        
+        }
+
+
+    }
+}
