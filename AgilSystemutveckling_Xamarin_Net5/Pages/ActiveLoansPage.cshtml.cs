5fd42d73e7747c4506a29f9a6aaa1a2a9851c73b
Mohamed EL Yahiaoui <Mohamed.el.yahiaoui@outlook.com>
2022-05-24 11:29:56 +0200

    Implemented a bit of blook user logic(user cant loan if blocked), created nav tap and page Active loans

---
+++ AgilSystemutveckling_Xamarin_Net5/Pages/ActiveLoansPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgilSystemutveckling_Xamarin_Net5.Pages
+{
+    public class ActiveLoansPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
