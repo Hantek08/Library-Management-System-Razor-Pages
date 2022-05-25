2230e89c36bc189533ab82630cec83b2a48d8b62
Mohamed EL Yahiaoui <Mohamed.el.yahiaoui@outlook.com>
2022-05-25 14:19:30 +0200

    Can now block and unblock user

--- AgilSystemutveckling_Xamarin_Net5/Pages/Admin/User/EditUserPage.cshtml.cs
+++ AgilSystemutveckling_Xamarin_Net5/Pages/Admin/User/EditUserPage.cshtml.cs
@@ -5,8 +5,21 @@ namespace AgilSystemutveckling_Xamarin_Net5.Pages.Admin.User
 {
     public class EditUserPageModel : PageModel
     {
+        public static Models.Users UserInfo = new Models.Users();
         public void OnGet(int id)
         {
+             UserInfo = Service.GetService.Get.GetAllUsers().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public void OnPostBlock()
+        {
+            UserInfo.Blocked = true;
+            Service.UpdateService.Update.User(UserInfo);
+        }
+        public void OnPostUnblock()
+        {
+            UserInfo.Blocked = false;
+            Service.UpdateService.Update.User(UserInfo);
         }
     }
 }
