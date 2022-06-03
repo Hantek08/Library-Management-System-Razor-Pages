9425ef512c90e1d17919765c0a758b45d49db923
Hanna Tekie <hantek90@gmail.com>
2022-06-03 14:10:40 +0200

    commented in Category, Report cshtml and moved print style in css folder and print javascript function in Javascript folder

---
+++ AgilSystemutveckling_Xamarin_Net5/wwwroot/js/print.js
@@ -0,0 +1,9 @@
+﻿//Javascript print function
+ function printContent(el){
+        var restorepage = document.body.innerHTML;
+    var printcontent = document.getElementById(el).innerHTML;
+    document.body.innerHTML = printcontent;
+    window.print();
+    document.body.innerHTML = printcontent;
+    history.go(0);
+    }
