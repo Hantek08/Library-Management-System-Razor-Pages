73a0ef64742708a7f22bfe5041c685c2839eae8c
Frans Nilsson Lidström <fransnilssonlidstrom@gmail.com>
2022-06-04 14:53:56 +0200

    - testpage

--- AgilSystemutveckling_Xamarin_Net5/Pages/testPages/Frans.cshtml.cs
+++ AgilSystemutveckling_Xamarin_Net5/Pages/testPages/Frans.cshtml.cs
@@ -5,10 +5,10 @@ namespace AgilSystemutveckling_Xamarin_Net5.Pages.testPages
 {
     public class FransModel : PageModel
     {
-        public static List<Models.History> history = new List<Models.History>();
+        
         public void OnGet()
         {
-            history = Service.GetService.Get.ActiveLoans(8);
+
         }
     }
 }
